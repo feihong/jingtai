e0292126bf6d2d3236cc8fd46ea8a0ba10bde511
Feihong Hsu <feihong.hsu@gmail.com>
2016-06-20 15:26:19 -0500

    Add create_project() function

--- jingtai/__init__.py
+++ jingtai/__init__.py
@@ -1,15 +1,32 @@
 __title__ = 'jingtai'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Feihong Hsu'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Feihong Hsu'
 __all__ = ['Site']
 
 
+import shutil
 from .site import Site
 from .compat import Path
 
 
 def create_project(name):
-    Path.mkdir(name)
-    # tbd
+    resources = Path(__file__).parent / 'resources'
+
+    root = Path(name)
+    root.mkdir()
+
+    (root / 'tasks.py').write_text(
+        (resources / 'tasks.py').read_text() % {'project_name': name}
+    )
+
+    site = root / 'site'
+    site.mkdir()
+
+    shutil.copy(str(resources / '404.html'), str(site))
+    shutil.copy(str(resources / 'index.html'), str(site))
+
+    templates = root / 'templates'
+
+    shutil.copy(str(resources / 'base.plim'), str(templates))
---
+++ jingtai/resources/tasks.py
@@ -0,0 +1,32 @@
+from invoke import task
+from jingtai import Site
+
+
+site = Site('/%(project_name)s/')
+
+
+@task
+def serve(ctx):
+    site.watch('site')
+    site.watch('templates')
+    site.serve(port=8000)
+
+
+@task
+def serve_build(ctx):
+    site.serve_build(port=8000)
+
+
+@task
+def build(ctx):
+    site.build()
+
+
+@task
+def clean(ctx):
+    site.clean()
+
+
+@task
+def publish(ctx):
+    site.publish()
